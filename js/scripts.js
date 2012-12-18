36791572f8f9b5ae3b96615170aa827db22487cc
Metro <jessicametro@gmail.com>
2012-12-18 02:35:56 -0600

    navigation underlines

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,25 @@ $(document).ready(function() {
 	$("#navStream").click(function() {
 		$(".content").hide();
 		$("#streamBody").show();
+		$(".tabs").removeClass("selected");
+		$("#navStream").addClass("selected");
 	});
 	
 	$("#navExplore").click(function() {
 		$(".content").hide();
 		$("#exploreBody").show();
+		$(".tabs").removeClass("selected");
+		$("#navExplore").addClass("selected");
+		
 	});
 	
 	$("#navNowPlaying").click(function() {
 		$(".content").hide();
 		$("#nothingPlayingBody").show();
+		$(".tabs").removeClass("selected");
+		$("#navNowPlaying").addClass("selected");
 	});
 
 });
+
+
