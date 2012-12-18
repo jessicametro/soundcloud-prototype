d209c3c71c9ec79916f10b848a163380b423b944
Metro <jessicametro@gmail.com>
2012-12-18 02:13:11 -0600

    navigation --> page

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,15 @@ $(document).ready(function() {
 		$(".content").hide();
 		$("#streamBody").show();
 	});
+	
+	$("#navExplore").click(function() {
+		$(".content").hide();
+		$("#exploreBody").show();
+	});
+	
+	$("#navNowPlaying").click(function() {
+		$(".content").hide();
+		$("#nothingPlayingBody").show();
+	});
 
 });
